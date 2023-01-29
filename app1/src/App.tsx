9cdbdd4e5aebbe4566c89ba92f92d7f7c8e0de83
Arnab <grinit10@gmail.com>
2023-01-29 13:29:11 +1100

    Fixing the app1

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,23 +1,25 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { Container, Grid } from "@mui/material";
 import Box from "@mui/material/Box";
-import React from "react";
+import React, { Suspense } from "react";
 
 const CounterAppOne = React.lazy(() => import("./components/CounterAppOne"));
 
 const App = () => (
-  <CssBaseline>
-    <Box sx={{ flexGrow: 1, textAlign: "center" }}>
-      <Container fixed>
-        <Grid container direction={"column"}>
-          <Box component="span" sx={{ p: 2, border: "1px dashed grey" }}>
-            App 1
-          </Box>
-          <CounterAppOne></CounterAppOne>
-        </Grid>
-      </Container>
-    </Box>
-  </CssBaseline>
+  <Suspense fallback={<div>Loading...</div>}>
+    <CssBaseline>
+      <Box sx={{ flexGrow: 1, textAlign: "center" }}>
+        <Container fixed>
+          <Grid container direction={"column"}>
+            <Box component="span" sx={{ p: 2, border: "1px dashed grey" }}>
+              App 1
+            </Box>
+            <CounterAppOne></CounterAppOne>
+          </Grid>
+        </Container>
+      </Box>
+    </CssBaseline>
+  </Suspense>
 );
 
 export default App;
